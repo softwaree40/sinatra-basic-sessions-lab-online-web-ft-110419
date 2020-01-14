d2718a1f878930b413698700454805d421e49b80
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-14 22:36:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+     configure do
+     enable :sessions
+     set :session_secret, "secret"
+  
+     end
+     
+    get "/" do 
+       
+    end
 end
